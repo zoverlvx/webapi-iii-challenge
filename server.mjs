e18e26acd9cf4634700cf3fd98a6aa992bd53170
Zach Overhulser <zoverhulser@gmail.com>
2019-05-08 21:16:11 +0000

    Gets all posts and users as well as specified user and post by id.

--- server.mjs
+++ server.mjs
@@ -1,12 +1,16 @@
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
+import usersRouter from "./routes/users";
+import postsRouter from "./routes/posts";
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 server.use(morgan("dev"));
+server.use("/api/users", usersRouter);
+server.use("/api/posts", postsRouter);
 
 server.get("/", (req, res) => {
 	res.send("Server running");
